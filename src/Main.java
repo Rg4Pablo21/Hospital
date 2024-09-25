8a875014ab33caab7add5dc0c667e13912d4e158
PG <gic438@gmail.com>
2024-09-25 08:10:30 -0600

    Teerminado el paciente

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
-import view.LoginView;
-import controller.loginController;
+import model.Enfermeras;
+import model.Salas;
+import model.Secretarias;
+import servicios.bdDoctores;
+import view.DoctorView;
+import model.paciente;
+
 
 import javax.swing.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
-        LoginView loginView= new LoginView();
-        loginController loginController = new loginController(loginView);
 
 
+        Enfermeras enfermeras = new Enfermeras();
+        Salas salas = new Salas();
+        Secretarias secretarias = new Secretarias();
+
+        bdDoctores bdDoctoresService = new bdDoctores();
+
+
+
+
+        DoctorView doctorView = new DoctorView();
+
+
+        System.out.println("Enfermeras: " + enfermeras);
+        System.out.println("Salas: " + salas);
+        System.out.println("Secretarias: " + secretarias);
+        System.out.println("paciente");
+
+
+        doctorView.show();
     }
 }
--- src/view/DoctorView.java
+++ src/view/DoctorView.java
@@ -5,42 +5,51 @@ import java.awt.*;
 
 public class DoctorView extends JFrame {
     private int[] pantalla = {1300, 800};
+
     public DoctorView() {
+
 
         setTitle("Perfil del doctor");
         setSize(pantalla[0], pantalla[1]);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
+
         JPanel headerPanel = new JPanel();
         headerPanel.setPreferredSize(new Dimension(1300, 60));
         headerPanel.setBackground(Color.DARK_GRAY);
         headerPanel.setLayout(new BorderLayout());
+
 
         JLabel hospitalLabel = new JLabel("Hospital Santa Catalina");
         hospitalLabel.setForeground(Color.WHITE);
         hospitalLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0)); // Espaciado en el lado izquierdo
         headerPanel.add(hospitalLabel, BorderLayout.WEST);
 
+
         JPanel userPanel = new JPanel();
         userPanel.setLayout(new GridBagLayout());
         userPanel.setBackground(Color.DARK_GRAY);
+
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.insets = new Insets(5, 5, 5, 5);
 
+
         JPanel logoPanel = new JPanel();
         logoPanel.setBackground(Color.YELLOW);
         logoPanel.setPreferredSize(new Dimension(100, 100));
         userPanel.add(logoPanel, gbc);
 
+
         gbc.gridx = 1;
         gbc.gridy = 0;
-        JLabel doctorNameLabel = new JLabel("Diego Villatoro");
+        JLabel doctorNameLabel = new JLabel("Pablo Girón");
         doctorNameLabel.setForeground(Color.WHITE);
         userPanel.add(doctorNameLabel, gbc);
+
 
         gbc.gridx = 1;
         gbc.gridy = 1;
@@ -48,12 +57,39 @@ public class DoctorView extends JFrame {
         doctorSpecialtyLabel.setForeground(Color.WHITE);
         userPanel.add(doctorSpecialtyLabel, gbc);
 
+
         headerPanel.add(userPanel, BorderLayout.EAST);
-
-        add(headerPanel, BorderLayout.NORTH);
-
+        add(componentesLateral(),BorderLayout.WEST);
         setVisible(true);
     }
 
+    private JPanel componentesLateral(){
+        JPanel menuPanel = new JPanel();
+        menuPanel.setPreferredSize(new Dimension(250,pantalla[1]));
+        menuPanel.setBackground(Color.darkGray);
 
+        JPanel menu = new JPanel();
+        menu.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx= 0;
+
+        menu.add(op("Opcion 1"),gbc);
+        menu.add(op("Opcion 2"),gbc);
+        menu.add(op("Opcion 3"),gbc);
+        menu.add(op("Opcion 4"),gbc);
+        menu.add(op("Opcion 5"),gbc);
+
+        menuPanel.add(menu);
+        return menuPanel;
+    }
+
+    private JButton op (String texto){
+        JButton op = new JButton(texto);
+        op.addActionListener(e ->{
+            System.out.println(texto);
+        });
+        return op;
+    }
 }
